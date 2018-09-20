b06023b830be803303f24cbaafa97a14cdb0ae7a
Andres Zapata <me@azapata.io>
2018-09-20 10:27:29 -0500

    Add sass, bulma, sass-loader, css-loader and autoprefixer

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -3,6 +3,7 @@ import { Route, NavLink } from "react-router-dom"
 import NewsFeed from "Components/Pages/NewsFeed"
 import People from "Components/Pages/People"
 import Profile from "Components/Pages/Profile"
+import "./App.scss"
 
 const App = () => (
   <div className="App">
@@ -18,7 +19,7 @@ const App = () => (
       <li>More</li>
     </ul>
     <Route path="/feed" component={NewsFeed} />
-    <Route path="/profile" component={Profile} />
+    <Route path="/people" component={People} />
   </div>
 )
 
