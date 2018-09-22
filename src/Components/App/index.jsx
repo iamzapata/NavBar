d9d0f11de7d2186a74302a8d1e84db2c35343261
Andres Zapata <me@azapata.io>
2018-09-22 16:50:58 -0500

    Extract navbar to own file

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,23 +1,14 @@
 import React from "react"
-import { Route, NavLink } from "react-router-dom"
+import { Route } from "react-router-dom"
 import NewsFeed from "Components/Pages/NewsFeed"
 import People from "Components/Pages/People"
 import Profile from "Components/Pages/Profile"
+import NavBar from "Components/Navbar"
 import "./App.scss"
 
 const App = () => (
   <div className="App">
-    <ul>
-      <li>
-        <NavLink to="/feed">News</NavLink>
-      </li>
-      <li>
-        <NavLink to="/people">People</NavLink>
-      </li>
-      <li>Food</li>
-      <li>Admin</li>
-      <li>More</li>
-    </ul>
+    <NavBar />
     <Route path="/feed" component={NewsFeed} />
     <Route path="/people" component={People} />
   </div>
