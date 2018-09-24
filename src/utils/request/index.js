23bb6afc44765344c33f8f8ed747867bda6c0aa1
Andres Zapata <me@azapata.io>
2018-09-24 00:20:58 -0500

    Create helper request function

---
+++ src/utils/request/index.js
@@ -0,0 +1,19 @@
+const checkStatus = response => {
+  if (response.ok) {
+    return response
+  }
+  throw new Error(response.status)
+}
+
+function request(url) {
+  const requestUrl = `api/${url}`
+
+  return fetch(requestUrl)
+    .then(checkStatus)
+    .then(response => response.json())
+    .catch(err => {
+      throw Error(err.message)
+    })
+}
+
+export default request
