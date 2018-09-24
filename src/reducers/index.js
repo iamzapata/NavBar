b8b2ee722978d8c4f14592cdcad89b5768dacc3c
Andres Zapata <me@azapata.io>
2018-09-24 00:58:31 -0500

    Add NewsFeed store to global store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux"
 import Theme from "Components/Common/ThemeSwitcher/reducers"
+import NewsFeed from "Pages/NewsFeed/reducers"
 
 export default combineReducers({
-  Theme
+  Theme,
+  NewsFeed
 })
