982958ea47d49b78c74a1ca43c3c783e7cf1348d
Andres Zapata <me@azapata.io>
2018-09-24 00:20:03 -0500

    Add babel runtime/async plugins

---
+++ src/Components/Pages/NewsFeed/actionTypes/ActionTypes.spec.js
@@ -0,0 +1,7 @@
+import ActionTypes from "./index.js"
+
+describe("ActionTypes", () => {
+  it("Should have a SWITCH_THEME action type defined", () => {
+    expect(ActionTypes.SWITCH_THEME).toBeTruthy()
+  })
+})
