699e668335afa8b49a626b56a543d690c5870039
Andres Zapata <me@azapata.io>
2018-09-23 23:38:10 -0500

    Connect <App /> to theme switch

--- src/Components/App/App.spec.js
+++ src/Components/App/App.spec.js
@@ -1,14 +1,21 @@
 import React from "react"
 import { render, cleanup } from "react-testing-library"
 import { BrowserRouter as Router } from "react-router-dom"
+import configureMockStore from "redux-mock-store"
+import { Provider } from "react-redux"
 import App from "./index"
+
+const mockStore = configureMockStore()
+const store = mockStore({ Theme: { themeName: "Pinterest" } })
 
 describe("<App />", () => {
   afterEach(cleanup)
   it("Should render self", () => {
     const { container } = render(
       <Router>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </Router>
     )
     expect(container.firstChild).toBeTruthy()
--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,17 +1,41 @@
 import React from "react"
-import { Route } from "react-router-dom"
+import { string } from "prop-types"
+import { Route, Switch, withRouter } from "react-router-dom"
 import NewsFeed from "Components/Pages/NewsFeed"
 import People from "Components/Pages/People"
 import Profile from "Components/Pages/Profile"
 import NavBar from "Components/Navbar"
+import PageContainer from "Pages/PageContainer"
+import ThemeSwitcher from "Components/Common/ThemeSwitcher"
+import { connect } from "react-redux"
+import themeSelector from "selectors/themeSelector"
 import "./App.scss"
 
-const App = () => (
-  <div className="App">
+const App = ({ theme }) => (
+  <div className={`App ${theme}__Theme`}>
     <NavBar />
-    <Route path="/feed" component={NewsFeed} />
-    <Route path="/people" component={People} />
+    <Switch>
+      <Route path="/" exact component={NewsFeed} />
+      <Route path="/feed" component={NewsFeed} />
+      <Route path="/people" component={People} />
+      <Route path="/profile/:user" component={Profile} />
+      <Route
+        path="/*"
+        component={() => <PageContainer>Not Yet Implemented</PageContainer>}
+      />
+    </Switch>
+    <footer>
+      <ThemeSwitcher switchTheme={() => null} />
+    </footer>
   </div>
 )
 
-export default App
+const mapStateToProps = state => ({
+  theme: themeSelector(state)
+})
+
+App.propTypes = {
+  theme: string.isRequired
+}
+
+export default withRouter(connect(mapStateToProps)(App))
