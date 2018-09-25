29d3224c010e7f0278aaf6263875aa3d976782a5
Andres Zapata <me@azapata.io>
2018-09-24 21:25:31 -0500

    Adjust burguer button, add tests to handle burger menu open/close

--- src/Components/Navbar/NavBar.spec.js
+++ src/Components/Navbar/NavBar.spec.js
@@ -1,9 +1,10 @@
 import React from "react"
-import { render, cleanup } from "react-testing-library"
+import { render, cleanup, fireEvent } from "react-testing-library"
 import configureMockStore from "redux-mock-store"
 import { Provider } from "react-redux"
 import { BrowserRouter as Router } from "react-router-dom"
 import NavBar from "./index"
+import "jest-dom/extend-expect"
 
 const mockStore = configureMockStore()
 const store = mockStore({ Theme: { themeName: "Pinterest" } })
@@ -20,5 +21,25 @@ describe("<NavBar />", () => {
     )
     expect(container.firstChild).toBeTruthy()
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it("Should toggle burger menu", () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Router>
+          <NavBar />
+        </Router>
+      </Provider>
+    )
+    const burgerButton = container.querySelector("button")
+    const navbarMenu = container.querySelector(".navbar-menu")
+
+    fireEvent.click(burgerButton)
+    expect(burgerButton).toHaveClass("is-active")
+    expect(burgerButton).toHaveClass("is-active")
+
+    fireEvent.click(burgerButton)
+    expect(burgerButton).not.toHaveClass("is-active")
+    expect(navbarMenu).not.toHaveClass("is-active")
   })
 })
