39512b317eb93e3d4215b4b1ce0e650397058c10
Andres Zapata <me@azapata.io>
2018-09-25 16:10:22 -0500

    Add <UpcomingEvents /> reducers, actions, action-types

---
+++ src/Components/Pages/NewsFeed/actionTypes/upcomingEvents/index.js
@@ -0,0 +1,9 @@
+import keyMirror from "keymirror"
+
+const ActionTypes = keyMirror({
+  GET_UPCOMMING_EVENTS_REQUEST: null,
+  GET_UPCOMMING_EVENTS_SUCCESS: null,
+  GET_UPCOMMING_EVENTS_FAILURE: null
+})
+
+export default ActionTypes
