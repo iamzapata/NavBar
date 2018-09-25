0d59892cabc5b0ff288aff4a01108d965eaad9b2
Andres Zapata <me@azapata.io>
2018-09-24 20:03:55 -0500

    Add Should toggle theme test to <ThemeSwitcher />

--- src/Components/Common/ThemeSwitcher/ThemeSwitcher.spec.js
+++ src/Components/Common/ThemeSwitcher/ThemeSwitcher.spec.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, cleanup } from "react-testing-library"
+import { render, cleanup, fireEvent } from "react-testing-library"
 import { Provider } from "react-redux"
 import { ThemeSwitcher } from "./index"
 
@@ -14,5 +14,28 @@ describe("<ThemeSwitcher />", () => {
     const { container } = render(<ThemeSwitcher {...createProps()} />)
     expect(container.firstChild).toBeTruthy()
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it("Should toggle current theme", () => {
+    let props = {
+      ...createProps(),
+      theme: "Pinterest"
+    }
+    const { container, rerender } = render(<ThemeSwitcher {...props} />)
+
+    const checkbox = container.querySelector("#themeSwitcherButton")
+
+    fireEvent.click(checkbox)
+    expect(props.switchTheme).toHaveBeenCalledTimes(1)
+    expect(props.switchTheme).toHaveBeenCalledWith("Square")
+
+    props = {
+      ...createProps(),
+      theme: "Square"
+    }
+    rerender(<ThemeSwitcher {...props} />)
+    fireEvent.click(checkbox)
+    expect(props.switchTheme).toHaveBeenCalledTimes(1)
+    expect(props.switchTheme).toHaveBeenCalledWith("Pinterest")
   })
 })
