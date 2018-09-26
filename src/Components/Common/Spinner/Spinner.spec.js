ba9563b90df41d9c55d5d85dff3c0090738d66ea
Andres Zapata <me@azapata.io>
2018-09-25 20:50:22 -0500

    Move jes-dom import

--- src/Components/Common/Spinner/Spinner.spec.js
+++ src/Components/Common/Spinner/Spinner.spec.js
@@ -1,12 +1,17 @@
 import React from "react"
 import { render, cleanup } from "react-testing-library"
+import "jest-dom/extend-expect"
 import Spinner from "./index"
 
 describe("<Spinner />", () => {
   afterEach(cleanup)
   it("Should render self", () => {
-    const { container } = render(<Spinner />)
+    const { container } = render(<Spinner isLoading={true} />)
     expect(container.firstChild).toBeTruthy()
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it("Should be visible when isLoading true", () => {
+    const { container } = render(<Spinner isLoading={true} />)
   })
 })
