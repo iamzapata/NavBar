f79c96afcc769a22cf3378b71e9b6e9efec8690e
Andres Zapata <me@azapata.io>
2018-09-25 22:40:48 -0500

    Crete <Event /> and add styling

--- src/Components/Pages/NewsFeed/Components/UpcommingEvents/Event/index.jsx
+++ src/Components/Pages/NewsFeed/Components/UpcommingEvents/Event/index.jsx
@@ -1,13 +1,26 @@
 import React from "react"
 import { shape, string } from "prop-types"
+import { isSameWeek, format } from "date-fns"
+import "./Event.scss"
 
 const Event = ({ event: { name, date, location } }) => {
-  return <div className="Event" />
+  return (
+    <div className="Event">
+      <p className="Event__Name title is-size-6">{name}</p>
+      <p className="subtitle is-size-7">
+        <span className="Event__Date">{date}</span>
+        {", "}
+        <span className="Event__Location">{location}</span>
+      </p>
+    </div>
+  )
 }
 
 Event.propTypes = {
   event: shape({
-    event: string
+    name: string,
+    date: string,
+    location: string
   })
 }
 
