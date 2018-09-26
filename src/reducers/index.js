724e7e71ec2369c99f963356770f982afa475709
Andres Zapata <me@azapata.io>
2018-09-25 22:03:59 -0500

    Add UpcomingEvents to global store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux"
 import Theme from "Components/Common/ThemeSwitcher/reducers"
 import NewsFeed from "Pages/NewsFeed/reducers/newsFeed"
 import UserProfile from "Pages/NewsFeed/reducers/userProfile"
+import UpcomingEvents from "Pages/NewsFeed/reducers/upcomingEvents"
 
 export default combineReducers({
   Theme,
   NewsFeed,
-  UserProfile
+  UserProfile,
+  UpcomingEvents
 })
