29527e4977a45746c285fcaaba34f32c30375dcf
Andres Zapata <me@azapata.io>
2018-09-25 22:45:12 -0500

    Fix file misplacement error

--- src/Components/Pages/NewsFeed/Components/UpcommingEvents/Event/helpers/index.js
+++ src/Components/Pages/NewsFeed/Components/UpcommingEvents/Event/helpers/index.js
@@ -1,7 +1,7 @@
 import { format, isSameWeek } from "date-fns"
 
-export default const friendlyEventDate = date => {
+export default (friendlyEventDate = date => {
   if (isSameWeek(date, new Date())) return format(date, "dddd, h A")
 
   return format(date, "MMMM D, h A")
-}
+})
